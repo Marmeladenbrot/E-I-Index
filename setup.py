b5f2988f6b6bdc505f26fef8889b96cbf0a2e75c
Vega <marmeladenbrot@users.noreply.github.com>
2020-04-27 15:42:01 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='E-I-Index',
+    version='0.1',
+    packages=['E-I-Index_pkg'],
+    url='https://github.com/Marmeladenbrot/E-I-Index',
+    license='',
+    author='Matthias Reuße',
+    author_email='marmeladenbrot@users.noreply.github.com',
+    description='calculation of global, cluster and single node E-I Index based on Krackhardt & Stern (1988), https://doi.org/10.2307/2786835'
+)
